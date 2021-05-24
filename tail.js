3322ab2c81995e5e443f3973fe17c1a86e3c77f4
dextergb <dextergb@rogers.com>
2021-05-24 22:15:09 +0000

    create a tail function that returns all but first array using assertEqual

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `❌ Assertion Failed: ${actual} !=== ${expected}`;
+  }
+};
+
+const tail = function(array) {
+  // let result = [];
+  // for (let i = 1; i < array.length; i++) {
+  //   result.push(array[i]);
+  // }
+  // return result;
+  return array.slice(1);
+};
+
+
+
+// TEST CODE
+console.log(assertEqual(tail([5,6,7]), 5));
+console.log(assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(tail([5,6,7]), [6,7]));
