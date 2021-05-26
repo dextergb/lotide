77e724782c6252912ae1cab90924eb63d9efa90e
dextergb <dextergb@rogers.com>
2021-05-26 19:13:25 +0000

    refactor with assistance

--- without.js
+++ without.js
@@ -1,12 +1,10 @@
 const eqArrays = function(firstArray, secondArray) {
-  let test = true;
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
-      test = false;
-      return test;
+      return false;
     }
   }
-  return test;
+  return true;
 };
 
 const assertArraysEqual = function(actual, expected) {
@@ -17,20 +15,36 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const without = function (originalArray, removedItems){
-  let newArray = [];
-  for(const arr of originalArray){
-    newArray.push(arr);
-  }
-  for (let i = 0; i < removedItems.length; i++){
-    for (let j = 0; j < originalArray.length; j++){
-      if (removedItems[i] === originalArray[j]){
-        newArray.splice(j, 1);
-      }
+const without = function (originalArray, removedItems) {
+  let newArray = []; // creating empty array to push items into later
+  for (const element of originalArray) { // going through each element of originalArray
+    if (removedItems.includes(element)) { // checks if the element that we are looping is found within the removedItems array
+      // console.log('element is inlcuded and wont be added', element);
+    } else { // if the element is not found within removedItems then push into newArray
+      newArray.push(element)
     }
   }
   return newArray;
-}
+};
 
 console.log(without([1, 2, 3], [1]));
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
+
+
+
+
+
+// const without = function (originalArray, removedItems){
+//   let newArray = [];
+//   for(const arr of originalArray){
+//     newArray.push(arr);
+//   }
+//   for (let i = 0; i < removedItems.length; i++){
+//     for (let j = 0; j < originalArray.length; j++){
+//       if (removedItems[i] === originalArray[j]){
+//         newArray.splice(j, 1);
+//       }
+//     }
+//   }
+//   return newArray;
+// }
