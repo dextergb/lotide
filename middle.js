1179e447d60c3cb097dc462ceb5e31c06748b128
dextergb <dextergb@rogers.com>
2021-05-26 17:06:11 +0000

    initial commit

---
+++ middle.js
@@ -0,0 +1,32 @@
+const eqArrays = function(firstArray, secondArray) {
+  let test = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      test = false;
+      return test;
+    }
+  }
+  return test;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(array) {
+  const middleIndex = Math.floor(array.length / 2);
+  const arr = [];
+  if (array.length <= 2) {
+    return arr;
+  } else if (array.length % 2 === 0) {
+    arr.push(array[middleIndex - 1]);
+    arr.push(array[middleIndex]);
+  } else {
+    arr.push(array[middleIndex]);
+  }
+  return arr;
+};
