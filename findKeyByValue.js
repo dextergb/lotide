0a2c31bfc6b6c7564ef3e96091065f444bb96618
dextergb <dextergb@rogers.com>
2021-05-27 00:14:00 +0000

    initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// implement function findKeyByValue which takes in an object and a value
+// scan the object
+// return the first key which contains the given value
+// if no key with given value is found then
+// return undefined
+
+// utilize object.keys():
+// returns an array of a given object's own enumerable property names,
+// iterated in the same order that a normal loop would.
+
+const findKeyByValue = function(object, value) {
+  // console.log('object.keys:', Object.keys(object))
+  for (const key of Object.keys(object)) {
+    // console.log(key)
+    // console.log(object[key])
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+// const allKeys = Object.keys(object);
+// for (const key of allKeys) {
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
