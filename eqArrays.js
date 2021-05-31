e383f29e3c9d69ceadb97dded7cd33714388314b
dextergb <dextergb@rogers.com>
2021-05-31 17:42:22 +0000

    refactor and add eqArrays test

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual')
-
 const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
